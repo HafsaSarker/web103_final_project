761efbcc646afc99f0cc26abe3f061bb3415a73c
HafsaSarker <hafsahaqs@gmail.com>
2023-11-02 14:28:41 -0400

    add initial files

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="hover:bottom-0 border-8  flex flex-col">
+      <h1 className="bg-blue">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
