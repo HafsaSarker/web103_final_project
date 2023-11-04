9c2797108a09acf7cc8fb7a471b132b21278a7aa
HafsaSarker <hafsahaqs@gmail.com>
2023-11-03 20:05:20 -0400

    modify login page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Login() {
+function Register() {
   return (
     <div className="min-h-full min-w-full">
       <section className="bg-primary-50">
@@ -19,9 +19,25 @@ function Login() {
           <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-                Welcome Back
+                Create and account
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    for="username"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
+                  >
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    className="bg-gray-50 border-2 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 focus:border-2    focus:outline-none block w-full p-2.5"
+                    placeholder="funnyBunny20"
+                    required
+                  />
+                </div>
                 <div>
                   <label
                     for="email"
@@ -55,19 +71,41 @@ function Login() {
                   />
                 </div>
 
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="terms"
+                      aria-describedby="terms"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                      required
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label for="terms" className="font-light text-gray-500">
+                      I accept the{" "}
+                      <a
+                        className="font-medium text-primary-600 hover:underline"
+                        href="#"
+                      >
+                        Terms and Conditions
+                      </a>
+                    </label>
+                  </div>
+                </div>
                 <button
                   type="submit"
                   className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 >
-                  Login
+                  Create an account
                 </button>
                 <p className="text-sm font-light text-gray-500">
-                  Don't have an account?{" "}
+                  Already have an account?{" "}
                   <a
                     href="#"
                     className="font-medium text-primary-600 hover:underline"
                   >
-                    Register here
+                    Login here
                   </a>
                 </p>
               </form>
@@ -79,4 +117,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Register;
